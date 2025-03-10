b633143e82626eb346a81b1c3f7ed25af5901c97
VaidaRubyte <73707561+VaidaRubyte@users.noreply.github.com>
2025-03-10 11:43:26 +0000

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -8,14 +8,39 @@ module.exports = override(
       resourceRegExp: /^fs$/,
     })
   ),
-  // Add fallbacks for path, crypto, stream, and vm modules
+  // Add fallbacks for path, crypto, and stream modules if necessary
   (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      path: require.resolve('path-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      vm: require.resolve('vm-browserify'), 
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        path: require.resolve('path-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        vm: require.resolve('vm-browserify'),
+      },
+    };
+    return config;
+  }
+);const { override, addWebpackPlugin } = require('customize-cra');
+const webpack = require('webpack');
+
+module.exports = override(
+  // Ignore fs module
+  addWebpackPlugin(
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^fs$/,
+    })
+  ),
+  // Add fallbacks for path, crypto, and stream modules if necessary
+  (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        path: require.resolve('path-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        vm: require.resolve('vm-browserify'),
+      },
     };
     return config;
   }
